c09a623321ada3b56194d333c0150f86d049fe9e
root <root@localhost.localdomain>
2018-07-16 21:24:44 +0100

    more stuff

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, redirect
-import sqlite3
+from flask import Flask, request, redirect, send_from_directory
+import sqlite3, os
+
+filedir = "./uploads/"
 
 app = Flask(__name__)
 
@@ -12,7 +14,24 @@ def app_redir():
 	url = request.args['url']
 	return redirect(url)
 
+@app.route("/js",methods=['GET'])
+def app_js():
+	if 'src' in request.args:
+		return "<script src=\"%s\"/>" % request.args['src'], 200
+	elif 'script' in request.args:
+		return "<script>%s</script>" % request.args['script'], 200
+	else:
+		return "src?script?", 200
 
+@app.route("/files/<path:path>", methods=['GET'])
+def app_file_downl(path):
+	return send_from_directory(filedir, path, as_attachment=True)
+
+@app.route("/files/<filename>",methods=['POST'])
+def app_file_upl(filename):
+	with open(os.path.join(filedir,filename),"wb") as f:
+		f.write(request.data)
+	return "", 201 # 201 CREATED
 
 
 if __name__ == "__main__":
